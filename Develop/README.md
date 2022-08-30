0c5157c0da35a8147e7443320dcf5e36222de642
jam <jaymee602@gmail.com>
2022-08-29 19:37:49 -0700

    it's working!

---
+++ Develop/README.md
@@ -0,0 +1,30 @@
+# README Generator
+  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+  ## Description
+  Use this to generate a README.md file for your project
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  npm i, node index to start
+
+  ## Usage
+  run npm i, then node index and answer the questions to create a README.md file for your project
+
+  ## License
+  Click for info about this license: [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+  ## Contributing
+  Please be kind!
+
+  ## Tests
+  N/A
+
+  ## Questions
+  Github: (https://github.com/jaymee602)
+  Email: N/a
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,141 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project: (required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions for your project: (required)',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter installation instructions for your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use: (required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter usage instructions/examples for your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select the license you are using for your project:',
+            choices: [
+                'Apache License 2.0',
+                'GNU General Public License v3.0',
+                'MIT License',
+                'BSD 2-Clause "Simplified" License',
+                'BSD 3-Clause "New" or "Revised" License',
+                'Boost Software License 1.0',
+                'Creative Commons Zero v1.0 Universal',
+                'Eclipse Public License 1.0',
+                'GNU Affero General Public License v3.0',
+                'GNU General Public License v2.0',
+                'GNU Lesser General Public License v3.0',
+                'Mozilla Public License 2.0',
+                'The Unlicense',
+                'No license'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter contributing guidelines for your project:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter testing instructions for your project:'
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter your Github username: (required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email:'
+        }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    ]);
+}; 
 
-// TODO: Create a function to initialize app
-function init() {}
+// write README file
+const writeToFile = data => {
+    fs.writeFile('./README.md', data, err => {
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your README file has been created!");
+        };
+    });
+};
 
-// Function call to initialize app
+// function to initialize app
+function init() {
+    questions()
+    .then(answers => {
+        return generateMarkdown(answers);
+    })
+    .then(data => {
+        return writeToFile(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
+
+// function call to initialize app
 init();
